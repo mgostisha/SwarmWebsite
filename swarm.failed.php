b809fe89ff83ddb5599d3cf9b3d8921202182fc0
Martin Gostisha <martingostisha@gmail.com>
2014-05-27 13:51:20 -0500

    New failed page

--- swarm.failed.php
+++ swarm.failed.php
@@ -3,6 +3,7 @@
 	$errMsg = $_GET['errMsg'];
 
 	echo "Your parameters were not passed successfully. Error Message: ".$errMsg;
-	echo "\r\nClick <a href='http://www.astro.wisc.edu/~gostisha/swarm.html'>here</a> to return to the Swarm website.";
+	echo "<br>";
+	echo "Click <a href='http://www.astro.wisc.edu/~gostisha/swarm.html'>here</a> to return to the Swarm website.";
 
 ?>
